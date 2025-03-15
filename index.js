0ae7d6aff09a4364379f9cd9de8efa9f0d30bf72
subhajit_dev <146475311+subhajit1311-dev@users.noreply.github.com>
2025-03-15 12:20:53 +0530

    add dotenv support and update .gitignore

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+import 'dotenv/config'
+//console.log(process.env)
+
 import express from 'express'
 const app = express()
-const port=3000
+const port= process.env.PORT||3000
 // app.get("/",(req,res)=>{
 //     res.send("Hello from hitesh and his tea")
 // })
